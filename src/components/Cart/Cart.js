87b9e817a53b16afffea9ece9f5083ca479ab631
Viviana <vivianafb15@gmail.com>
2022-02-28 20:31:37 -0300

    Desafio10

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,29 +2,34 @@ import React, { useContext } from "react";
 import Item from "../Item/Item";
 import "./Cart.css";
 import { CarritoContext } from "../../context/CartProvider";
+import { Link } from "react-router-dom";
 function Cart() {
-  const { cartItems, clear,removeItem } = useContext(CarritoContext);
-
-  return (
+  const { cartItems, clear, removeItem } = useContext(CarritoContext);
+  return cartItems.length ? (
     <div>
-     
-
-      <div className="product__grid">
+      <div className="cart__grid">
         {cartItems.map((e) => (
-          <section className="items" >
-            <div className="items__content container container--pall">
+          <section className="cartitems">
+            <div className="cartitems__content container container--pall">
               <div>
-                <button className="deleteItemFromCart" onClick={() =>removeItem(e.id)}>x</button>
+                <button
+                  className="deleteItemFromCart"
+                  onClick={() => removeItem(e.id)}
+                >
+                  x
+                </button>
               </div>
-              {/* <div
-        className="item__image"
-        style={{ backgroundImage: `url(${e.image})` }}
-      ></div> */}
-              <div className="item__text">
-                <div className="item__title">{e.name}</div>
-                <div className="item__price">${e.price}</div>
+              <div className="cartitems__image"
+              style={{ backgroundImage: `url(${e.image})` }}>
+                
+              </div>
+              <div className="cartitems__text">
+                <div className="cartitems__title">{e.name}</div>
+                <div className="cartitems__price">${e.price}</div>
 
-                <div className="item__description">Cantidad: {e.quantity}</div>
+                <div className="cartitems__quantity">
+                  Cantidad: {e.quantity}
+                </div>
               </div>
             </div>
           </section>
@@ -33,6 +38,18 @@ function Cart() {
       <button className="deleteButton" onClick={clear}>
         Borrar todo
       </button>
+    </div>
+  ) : (
+    <div className="emptyCart">
+      <div className="textCart">
+        <img src="https://img.icons8.com/ios/50/000000/shopping-cart.png" />{" "}
+        <h3>Tu carrito esta vacio</h3>
+        <button className="shopNowButton">
+          <Link to={"/"} className="shopNow">
+            Comprar ahora
+          </Link>
+        </button>
+      </div>
     </div>
   );
 }
